576dae2f88314f003d5271dd6c0a57d101901877
kid443 <1033476775@qq.com>
2019-12-31 13:29:48 +0800

    页面配置

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    wx.getUserInfo({
+      success: function(res){
+        console.log(res)
+      }
+    })
     // 登录
     wx.login({
       success: res => {
